53d786e7fa3709dfa9b34f223eaed63a01fc1c55
AttawatTha <attawat.thaninsiripittayar@gmail.com>
2025-04-23 06:59:49 +0700

    add shadowing variables example

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -13,4 +13,13 @@ fn main() {
     println!("The value of x is: {x}");
 
     // shadowing variable
+    let y = 5;
+    let y = y + 1;
+
+    {
+        let y = y * 2;
+        println!("The value of y in the inner scope is: {y}");
+    }
+
+    println!("The value of y is: {y}");
 }
