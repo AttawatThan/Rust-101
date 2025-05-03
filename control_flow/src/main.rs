1f59de0f0f209a318eb97994a1d35ab25be43d98
AttawatTha <attawat.thaninsiripittayar@gmail.com>
2025-05-03 22:32:23 +0700

    Add if expression

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    // if Expressions
+    let number: i32 = 3;
+
+    if number < 5 {
+        println!("condition was true");
+    }
+    else {
+        println!("condition was false");
+    }
 }
